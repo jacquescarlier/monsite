8261219c53fd710c5161627d68bc70aa09fe7db6
jacquescarlier <jacques.carlier@gmail.com>
2024-04-16 17:15:22 +0200

    adding title attribute to search engines

--- src/container/nav/Navi.jsx
+++ src/container/nav/Navi.jsx
@@ -9,11 +9,11 @@ const Navi = () => {
             <div className='navigation'>
                 <span>Moteurs de recherche</span>
                 <ul className="socialMediaLinks">
-                    <li> <a href="https://www.google.fr" target="_blank" rel="noopener"> <img src="./logo/google.png" alt="logo google" aria-label="liens vers Google" /> </a> </li>
-                    <li><a href="https://www.qwant.com/?l=fr" target="_blank" rel="noopener"> <img src="./logo/qwant.png" alt="logo Qwant" aria-label="liens vers Qwant" /> </a></li>
-                    <li><a href="https://duckduckgo.com/?utm_source=bdmtools&utm_medium=siteweb&utm_campaign=duckduckgo" target="_blank" rel="noopener"><img src="./logo/duckduckgo.png" alt="logo Duckduckgo" aria-label="liens vers DuckduckGo" /></a></li>
-                    <li><a href="https://www.bing.com/" target="_blank" rel="noopener"><img src="./logo/bing.png" alt="logo Bing" aria-label="liens vers Bing" /></a></li>
-                    <li><a href="https://www.startpage.com//" target="_blank" rel="noopener"><img src="./logo/startpage.png" alt="logo Startpage" aria-label="liens vers startpage" /></a></li>
+                    <li> <a href="https://www.google.fr" target="_blank" rel="noopener" title= "Goolgle"> <img src="./logo/google.png" alt="logo google" aria-label="liens vers Google" /> </a> </li>
+                    <li><a href="https://www.qwant.com/?l=fr" target="_blank" rel="noopener" title="Qwant"> <img src="./logo/qwant.png" alt="logo Qwant" aria-label="liens vers Qwant" /> </a></li>
+                    <li><a href="https://duckduckgo.com/?utm_source=bdmtools&utm_medium=siteweb&utm_campaign=duckduckgo" target="_blank" rel="noopener" title="DuckDuckGo"><img src="./logo/duckduckgo.png" alt="logo Duckduckgo" aria-label="liens vers DuckduckGo" /></a></li>
+                    <li><a href="https://www.bing.com/" target="_blank" rel="noopener" title="Bing"><img src="./logo/bing.png" alt="logo Bing" aria-label="liens vers Bing" /></a></li>
+                    <li><a href="https://www.startpage.com//" target="_blank" rel="noopener" title="StartPage"><img src="./logo/startpage.png" alt="logo Startpage" aria-label="liens vers startpage" /></a></li>
                 </ul>
             </div>
         </div>
